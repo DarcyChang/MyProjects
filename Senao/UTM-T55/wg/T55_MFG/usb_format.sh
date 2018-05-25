a1a4d31fc9cbe10823e5dee3e312fdf038e2abfa
YOUR_NAME <YOUR_EMAIL>
2018-05-25 19:34:22 +0800

    [UTM-T55] Detecting USB dongle.

--- Senao/UTM-T55/wg/T55_MFG/usb_format.sh
+++ Senao/UTM-T55/wg/T55_MFG/usb_format.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+test_result_path="/etc/wg/log/diag/test_results.txt"
+test_result_failure_path="/etc/wg/log/diag/testresults-failure.txt"
+log_path="/etc/wg/log/diag/log.txt"
+
 extra_disk=$(lsblk|grep sd[bcdefg]|grep disk|awk '{print $1}')
 partition="1 2 3 4 5"
 ERR=0
+usb_num=0
+
+if [[ -z "$extra_disk" ]] ; then
+	ERR=1
+	echo "BURN_IN_TEST: No USB dongle" >> $test_result_failure_path
+else 
+	for disk in $extra_disk
+	do
+		usb_num=`expr $usb_num + 1`	
+	done
+	
+	if [ $usb_num -lt 2 ] ; then
+		ERR=1
+		echo "BURN_IN_TEST: USB dongle not enough, just only $usb_num" >> $test_result_failure_path
+	fi
+fi
+
 for disk in $extra_disk
 do
+	echo "[DEBUG] disk = $disk"
     for part in $partition
     do
         umount /dev/$disk$part > /dev/null 2>&1
@@ -14,7 +36,7 @@ do
     if [ "$?" != "0" ]; then
         ERR=1
         echo "Error: $disk MS-DOS partition fail"
-		echo "BURN_IN_TEST: $disk MS-DOS partition fail" >> /root/automation/testresults-failure.txt
+		echo "BURN_IN_TEST: $disk MS-DOS partition fail" >> $test_result_failure_path
         continue
     fi
 
@@ -22,7 +44,7 @@ do
     if [ "$?" != "0" ]; then
         ERR=1
         echo "Error: $disk partition full volume fail"
-		echo "BURN_IN_TEST: $disk partition full volume fail" >> /root/automation/testresults-failure.txt
+		echo "BURN_IN_TEST: $disk partition full volume fail" >> $test_result_failure_path
         continue
     fi
     sleep 1;
@@ -36,13 +58,13 @@ do
     if [ "$?" != "0" ]; then
         ERR=1
         echo "Error: $disk EXT3 format fail"
-		echo "BURN_IN_TEST: $disk EXT3 format fail" >> /root/automation/testresults-failure.txt
+		echo "BURN_IN_TEST: $disk EXT3 format fail" >> $test_result_failure_path
         continue
     fi
 done
 
 if [ "$ERR" == "1" ]; then
-    echo Format process is not completed.
+    echo "Format process is not completed."
 else
-    echo Format process is completed.
+    echo "Format process is completed."
 fi
